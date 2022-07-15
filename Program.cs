063e6ab9235c2dbce0b43bec5355a2ac9945bc40
Marat Islamov <Razr1629@gmail.com>
2022-07-15 20:28:32 +0300

    3

--- Program.cs
+++ Program.cs
@@ -104,11 +104,11 @@
                 Console.WriteLine();
             Double max =array[0];
             Double min =array[0];
-            for (int i =0; i< array.Length; i++)
-            {
-                if (array[i] > max) max = array[i];
-                else if (array[i]<min) min = array[i];
-            }
+                for (int i =0; i< array.Length; i++)
+                {
+                    if (array[i] > max) max = array[i];
+                    else if (array[i]<min) min = array[i];
+                }
             Console.WriteLine($"Максимальное значение равно: {max}");
             Console.WriteLine($"Минимальное значение равно: {min}");
             Console.WriteLine($"Разница равна {Math.Round(max-min, 2)}");
